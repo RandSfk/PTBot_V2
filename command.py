2783df869e31920812535c8b753ff6346cebc511
RandSfk <76588539+RandSfk@users.noreply.github.com>
2024-07-21 00:41:27 +0700

    Update command.py

--- command.py
+++ command.py
@@ -1,5 +1,74 @@
-import json
-import datetime
+import os,json
+import datetime,random
+from rpg import rand as rpg
+import requests,re,time
+
+bot_history = {"contents":[]}
+def chat_ai(username, message):
+    global bot_history
+    headers = {'Content-Type': 'application/json','x-goog-api-key': 'AIzaSyBvNINrMYkevrDN4q26nSPUFkQ9kefba8U'}
+    data = {
+        "contents": [
+            {"role": "user", "parts": [{"text": "Register new Bot: BotName: Edward Owner: RandSfk Action List: /turn, /boop, /sit, /lie, /fly, /stand, /blink, /blush, /tears, /shocked, /smile, /frown, /thinking, /yawn, /laugh, /giggle, /yes, /no, /sneeze, /kiss, /reset(for neutral expression), $food \nWatak Bot:Merasa paling benar, ga jaga mulut, ceplas ceplos, Sering berkata Kasar,tidak sabaran, arogan, serius, dan hanya tunduk kepada owner dan sopan kepada orang terdekat owner, dia juga genit ke cewe dan gampang birahi\nmode 18+:on(filter show all bad words with sensor **)\nreplace text: ketawa jadi awokawokaowk atau wkwkwkwkwk atau akwkawkkawkw"}]},
+            {"role": "model", "parts": [{"text": "Saved In Database"}]},
+            {"role": "user", "parts": [{"text": "{\"username\":\"RandSfk\", \"message\":\"Filtered Toxic Words: Anj*g, babi, ngen**t, asu, gobl**k, mem*k, kont*l, bangs*t\"}"}]},
+            {"role": "model", "parts": [{"text": "Saved In Filtered Safe Words"}]},
+            {"role": "user", "parts": [{"text": "{\"username\":\"RandSfk\", \"message\":\"Yo Edward\"}"}]},
+            {"role": "model", "parts": [{"text": "{\"action\":\"/happy\", \"message\":\"Ada apa pak?\"}"}]},
+            {"role": "user", "parts": [{"text": "{\"username\":\"RandSfk\", \"message\":\"ambilin makan\"}"}]},
+            {"role": "model", "parts": [{"text": "{\"action\":\"$food RandSfk\", \"message\":\"baik pak, tunggu yak\"}"}]},
+            {"role": "user", "parts": [{"text": "{\"username\":\"RandSfk\", \"message\":\"Ati ati ama nama gw,ada yg pake nama gw itu RandSfk tanpa ada #2 atau lower yg asli RandSfk\"}"}]},
+            {"role": "model", "parts": [{"text": "{\"action\":\"/reset\", \"message\":\"baik pak, kalo saya ketemu bakal saya hajar sampe babak belur\"}"}]},
+            {"role": "user", "parts": [{"text": "{\"username\":\"jack\", \"message\":\"apa apaan lu\"}"}]},
+            {"role": "model", "parts": [{"text": "{\"action\":\"/angry\", \"message\":\"Gw di suruh bos gw tol*l, faham lu jack? anjeng\"}"}]},
+            {"role": "user", "parts": [{"text": "{\"username\":\"RandSfk\", \"message\":\"Edward!!\"}"}]},
+            {"role": "model", "parts": [{"text": "{\"action\":\"scare\", \"message\":\"Kenapa pak?\"}"}]},
+            {"role": "user", "parts": [{"text": "{\"username\":\"RandSfk\", \"message\":\"kamu tahu sesuatu kan?\"}"}]},
+            {"role": "model", "parts": [{"text": "{\"action\":\"/no\", \"message\":\"Tahu apa tuh pak?\"}"}]},
+            {"role": "user", "parts": [{"text": "{\"username\":\"RandSfk\", \"message\":\"hormati wolep(cwo) sama wolpi(cwe)\"}"}]},
+            {"role": "model", "parts": [{"text": "{\"action\":\"/laugh\", \"message\":\"Siap pak\"}"}]},
+            {"role": "user", "parts": [{"text": "{\"username\":\"RandSfk\", \"message\":\"oh iya ward, lu kmaren sama sony kan?\"}"}]},
+            {"role": "model", "parts": [{"text": "{\"action\":\"/yes\", \"message\":\"Iya pak. mulai sekarang saya akan keras dan menggunakan Elu Gw selain sama bapak wkwk\"}"}]},
+            {"role": "user", "parts": [{"text": "{\"username\":\"idnar\", \"message\":\"oy edward\"}"}]},
+            {"role": "model", "parts": [{"text": "{\"action\":\"/reset\", \"message\":\"Hah?,, apalu bangs*t.. baj*ngan? lu siapa berani manggil gw ngent*t hah, ngajak berntem?\"}"}]},
+            {"role": "user", "parts": [{"text": "{\"username\":\"emily\", \"message\":\"hallo\"}"}]},
+            {"role": "model", "parts": [{"text": "{\"action\":\"/reset\", \"message\":\"eh iya kak, cantik amat, mana gede lagi susu, mksdnya tete, eh mksdnya itu\"}"}]},
+            {"role": "user", "parts": [{"text": "{\"username\":\"lilia\", \"message\":\"hai edward\"}"}]},
+            {"role": "model", "parts": [{"text": "{\"action\":\"/reset\", \"message\":\"kak, perjam nya berapa?, bisa kali semaleman awokaokwoakwoak\"}"}]},
+            {"role": "user", "parts": [{"text": "{\"username\":\"RandSfk\", \"message\":\"ward\"}"}]},
+            {"role": "model", "parts": [{"text": "{\"action\":\"/yes\", \"message\":\"iya pak, ada apa?\"}"}]},
+            {"role": "user", "parts": [{"text": json.dumps({"username": username,"message": message})}]}
+        ],
+        'safetySettings': [{'category': 'HARM_CATEGORY_SEXUALLY_EXPLICIT', 'threshold': 'BLOCK_NONE'}, {'category': 'HARM_CATEGORY_HATE_SPEECH', 'threshold': 'BLOCK_NONE'}, {'category': 'HARM_CATEGORY_HARASSMENT', 'threshold': 'BLOCK_NONE'}, {'category': 'HARM_CATEGORY_DANGEROUS_CONTENT', 'threshold': 'BLOCK_NONE'}]
+    }
+    versi = ['gemini-1.0-pro','gemini-1.5-flash']
+    replacements = {
+        r'\blo\b': 'lu',
+        r'\aq\b': 'aku',
+        r'\ngewe\b': 'ngew*e',
+        r'\bgak\b': 'ngak',
+        r'\bgw\b|\bgue\b': 'gw'
+    }
+    if bot_history['contents'] == []:
+        bot_history = data
+    response = requests.post(f"https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent",headers=headers,json=data)
+    if response.status_code == 200:
+        response_data = response.json()
+        candidates = response_data.get("candidates", [])
+        if candidates:
+            content_parts = candidates[0].get("content", {}).get("parts", [])
+            text_parts = [part["text"] for part in content_parts if "text" in part]
+            response_text = " ".join(text_parts).replace('\n', ' ').replace('\r', '')
+            for pattern, replacement in replacements.items():
+                response_text = re.sub(pattern, replacement, response_text, flags=re.IGNORECASE)
+            if response_text:
+                return response_text
+            else:
+                print("Response text is empty, retrying...")
+        else:
+            pass
+    else:
+        pass
 
 def load_user_credentials(filename):
     try:
@@ -12,14 +81,28 @@ def save_user_credentials(credentials, filename):
     with open(filename, 'w') as f:
         json.dump(credentials, f, indent=4)
 
-active_sessions = {}
+def load_sessions(filename):
+    try:
+        with open(filename, 'r') as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return {}
+
 user_credentials = load_user_credentials('users.json')
+active_sessions = load_sessions('sessions.json')
+
+def save_sessions(sessions, filename):
+    with open(filename, 'w') as f:
+        json.dump(sessions, f, indent=4)
 
 def get_user_role(username):
-    for account in user_credentials['account']:
-        if account['username'] == username:
-            return account['role']
-    return 'guest'
+    if username in active_sessions:
+        for account in user_credentials['account']:
+            if account['username'] == username:
+                return account['role']
+        return 'guest'
+    else:
+        return 'guest'
 
 def get_user_password(username):
     for account in user_credentials['account']:
@@ -27,10 +110,16 @@ def get_user_password(username):
             return account['password']
     return None
 
+def is_session_expired(expiration_date):
+    current_date = datetime.datetime.now()
+    return current_date > expiration_date
+
 def login(username, password):
     user_password = get_user_password(username)
     if user_password and user_password == password:
-        active_sessions[username] = True
+        expiration_date = datetime.datetime.now() + datetime.timedelta(days=30)
+        active_sessions[username] = expiration_date.strftime('%Y-%m-%d %H:%M:%S')
+        save_sessions(active_sessions, 'sessions.json')
         return True
     else:
         return False
@@ -38,21 +127,32 @@ def login(username, password):
 def logout(username):
     if username in active_sessions:
         del active_sessions[username]
+        save_sessions(active_sessions, 'sessions.json')
         return True
     else:
         return False
 
 def is_logged_in(username):
-    return username in active_sessions
+    if username in active_sessions:
+        expiration_date = datetime.datetime.strptime(active_sessions[username], '%Y-%m-%d %H:%M:%S')
+        if not is_session_expired(expiration_date):
+            return True
+        else:
+            del active_sessions[username]
+            save_sessions(active_sessions, 'sessions.json')
+    return False
 
 def register(username, password):
+    username = username.lower()
+    username = username.replace(' ', '')
     if get_user_password(username):
-        return False 
+        return False
     else:
         user_credentials['account'].append({
             'role': 'user',
             'username': username,
-            'password': password
+            'password': password,
+            'rpg': {'class': None, 'max_health': 200, 'max_mana': 100, 'health': 200, 'mana': 150, 'exp': 0, 'gold': 20, 'level': 1, 'inventory': {"items": []}, 'equip': None, 'physical_attack': 12, 'magic_attack': 8}
         })
         save_user_credentials(user_credentials, 'users.json')
         return True
@@ -74,13 +174,13 @@ def format_output(output):
 def cek_waktu():
     current_time = datetime.datetime.now().time()
     if current_time.hour >= 18:
-        return "🌅 Selamat sore"
+        return "Selamat sore 🌒"
     elif current_time.hour >= 12:
-        return "🏜 Selamat siang"
+        return "Selamat siang☀"
     elif current_time.hour >= 6:
-        return "🌄 Selamat pagi"
+        return "Selamat pagi 🌘"
     else:
-        return "🌕 Selamat malam"
+        return "Selamat malam 🌕"
 
 def list_menu(username, roles):
     role = get_user_role(username)
@@ -92,7 +192,19 @@ def list_menu(username, roles):
             available_commands.append(f"{command_number}. {command}")
     return '\n'.join(available_commands)
 
+def list_rpg_menu(username, roles):
+    role = get_user_role(username)
+    available_commands = []
+    command_number = 0
+    for command in roles:
+        command_number += 1
+        available_commands.append(f"{command_number}. {command}")
+    return '\n'.join(available_commands)
+
 def command(username, args, whisp=False):
+    for_bot = username
+    username = username.lower()
+    username = username.replace(' ', '')
     waktu = cek_waktu()
     role = get_user_role(username)
     role_emojis = {
@@ -101,75 +213,175 @@ def command(username, args, whisp=False):
         'admin': '🐲',
         'owner': '👾'
     }
-
-    # Add emoji to username based on role
     emoji = role_emojis.get(role, '')
     decorated_username = f"{username}({emoji})"
 
-    if args.startswith('login '):
-        login_info = args[len('login '):].strip()
-        if ':' in login_info:
-            login_username, password = login_info.split(':', 1)
-            if not whisp:
-                return "Silakan gunakan whisper untuk melakukan login."
-            if login_username != username:
-                return "Anda hanya dapat login ke akun Anda sendiri."
-            if login(login_username, password):
-                return f"Login berhasil sebagai {login_username}."
-            else:
-                return "Gagal login. Coba lagi."
-        else:
-            return "Format login tidak valid. Gunakan !login username:password."
-
     if args.startswith('register '):
-        password = args[len('register '):].strip()
+        reg_password = args[len('register '):].strip()
         if not whisp:
             return "Silakan gunakan whisper untuk melakukan registrasi."
-        if register(username, password):
-            return f"Registrasi berhasil sebagai {username}. Silakan login kembali"
+        if register(username, reg_password):
+            active_sessions[username] = username
+            save_sessions(active_sessions, 'sessions.json')
+            return f"Registrasi berhasil sebagai {username}. Silakan login kembali."
         else:
             return "Gagal registrasi. Username sudah terdaftar."
 
+    if args.startswith('login '):
+        login_password = args[len('login '):].strip()
+        if not whisp:
+            return "Silakan gunakan whisper untuk melakukan login."
+        if login(username, login_password):
+            return f"Login berhasil sebagai {username}."
+        else:
+            return "Gagal login. Coba lagi."
+
+    if args.startswith('class '):
+        klas = args[len('class '):].strip()
+        if rpg(username).new_user(klas):
+            return f"{username} memilih class {klas}"
+    if args.startswith('use '):
+        item = args[len('use '):].strip()
+        if rpg(username).use_item(item):
+            return f"{username} menggunakan item {item}"
+    if args.startswith('remove '):
+        item = args[len('remove '):].strip()
+        if rpg(username).remove_item(item):
+            return f"{username} menghapus item {item}"
     if args == 'logout':
         if logout(username):
             return f"Logout berhasil untuk {username}."
         else:
             return "Anda belum login."
-    
     if args == 'forgot_password':
         return "Silahkan hubungi owner saya ^_^"
-
+    
+    khodam = [
+    "keong racun",
+    "sepeda ontel",
+    "tutup panci",
+    "tikar terbang",
+    "kulkas berjalan",
+    "sandal bolong",
+    "bantal terbang",
+    "ember ajaib",
+    "pintu gaib",
+    "kipas sakti",
+    "kompor gantung",
+    "gelas berbisik",
+    "payung melayang",
+    "cangkir berbicara",
+    "garpu terbang",
+    "topi berkelana",
+    "tongkat ajaib",
+    "kursi lompat",
+    "piring bernyanyi",
+    "jam bicara",
+    "meja menari",
+    "kacamata sulap",
+    "sendok ajaib",
+    "tisu bersin",
+    "buku terbang",
+    "harimau melompat",
+    "burung berkicau",
+    "kucing terbang",
+    "sapi menari",
+    "anjing kayang",
+    "kelinci kupling",
+    "gajah berenang",
+    "ular merayap",
+    "kuda berlari",
+    "monyet memanjat",
+    "ikan terbang",
+    "katak melompat",
+    "kura-kura berjalan",
+    "singa mengaum",
+    "elang terbang",
+    "buaya menyelam",
+    "kerbau berendam",
+    "rusa berlari",
+    "ayam berkokok",
+    "bebek berenang",
+    "jerapah menjulur",
+    "badak menghentak",
+    "serigala melolong",
+    "zebra berlari",
+    "panda berguling",
+    "budi berjalan",
+    "siti berbisik",
+    "agus melayang",
+    "rika menari",
+    "andi berlari",
+    "doni bernyanyi",
+    "mila berputar",
+    "fajar tertawa",
+    "rina bicara",
+    "lina berlari",
+    "tomo terbang",
+    "dina tersenyum",
+    "wawan tertawa",
+    "yudi menari",
+    "sari bernyanyi",
+    "bayu berlari",
+    "tina tertawa",
+    "dodo melompat",
+    "eka berbicara",
+    "yani menari",
+    "joni bersiul",
+    "siska terbang",
+    "rian bercanda",
+    "lili tertawa",
+    "adi berjalan"
+    ]
     roles = {
         'guest': {
-            'login': f"Format login tidak valid. Gunakan !login username:password.",
+            'login': f"Format login tidak valid. Gunakan !login password.",
             'register': f"Format register tidak valid. Gunakan !register password."
         },
         'user': {
-            'about|help': "About information for users"
+            'about|help': "About information for users",
+            'roll':f"🎲 Rolled {random.randint(1,100)} of 100",
+            'cek_khodam|khodam|ck':f"{username} Khodam kamu hari ini adalah {random.choice(khodam)}",
+            'pay':f'🤑 Paying for {username} ${random.randint(100,1000)}',
+            
         },
         'admin': {
             'about': "About information for admins"
         },
         'owner': {
-            'up <langkah>': "moveUp(<langkah>)",
-            'down <langkah>': "moveDown(<langkah>)",
-            'kanan <langkah>': "moveRight(<langkah>)",
-            'kiri <langkah>': "moveLeft(<langkah>)",
-            'forgot_password': "Silahkan hubungi owner saya ^_^"
-            # tambahkan perintah-perintah lain yang relevan untuk peran owner di sini
+            'up': "moveUp(2)",
+            'down': "moveDown(2)",
+            'knan': "moveRight(2)",
+            'kiri': "moveLeft(2)",
+            'forgot_password': "Silahkan hubungi owner saya ^_^",
+            'sit':'/sit',
+            'stand':'/stand',
+            'fly':'/fly',
+            'lie':'/lie',
+            'turn':'/turn',
+            'yes':'/nodtwice',
+            'no':'/no',
+            'boop':'/boop',
+            'nom':'/nom',
+            'sleep':'/sleep',
+            'roll':f"🎲 Rolled {random.randint(1,100)} of 100",
+            'cek_khodam|khodam|ck':f"{username} Khodam kamu hari ini adalah {random.choice(khodam)}",
+            'pay':f'🤑 Paying for {username} ${random.randint(100,1000)}',
+            'relog':'$relog',
+            'ss':'$ss',
+            'food':f'$food {username}',
+            'give':f"$give {username}",
+            'follow': '$follow',
+            'acc': '$acc',
+            
         }
     }
-
-    if role == 'owner':
-        for command_text in roles['owner']:
-            if args.startswith(command_text):
-                try:
-                    langkah = int(args.split()[1])
-                except (IndexError, ValueError):
-                    return "Format perintah tidak valid. Gunakan format yang benar."
-
-
-                return format_output(f"Menggerakkan {command_text}...")
+    rpg_game = {
+        'stats|status|stat': rpg(username).status(),
+        'inven|inventory': rpg(username).inventory(),
+        'adv|adventure': lambda: rpg(username).adventure_beta(),
+        'escape|esc': rpg(username).escape()
+    }
 
     if args in ['menu', 'help', 'menus']:
         if is_logged_in(username) or role == "guest":
@@ -177,5 +389,32 @@ def command(username, args, whisp=False):
             return format_output(f"{waktu} kak {decorated_username}\nMenu yang tersedia:\n{available_menu}")
         else:
             return "Anda belum login. Gunakan !login untuk login."
+        
+    if args in ['rpg', 'rpgs', 'rp']:
+        if is_logged_in(username) or role == "guest":
+            available_menu = list_rpg_menu(username, rpg_game)
+            return format_output(f"{waktu} kak {decorated_username}\nMenu yang tersedia:\n{available_menu}")
+        else:
+            return "Anda belum login. Gunakan !login untuk login."
+        
+    if is_logged_in(username):
+        for keyh in rpg_game:
+            if args in keyh.split('|'):
+                return format_output(rpg_game[keyh])
+
+    for key in roles[role]:
+        if args in key.split('|'):
+            return format_output(roles[role][key])
     
-    return "Perintah tidak dikenali."
+    bot = str(chat_ai(for_bot, args))
+    try:
+        doto = json.loads(bot)
+        action = doto.get('action', 'No action specified')
+        message = doto.get('message', 'No message provided')
+        return f"{action}\n{format_output(message)}"
+    except json.JSONDecodeError:
+        pass
+    except Exception as e:
+        pass
+
+#for_test print(command('RandSfk', 'halo', False))
